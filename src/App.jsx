4d75c1ad17b3c190c9c09324318a1092b808b45d
Michael Hirt <hirtm14@gmail.com>
2023-05-11 16:50:56 -0400

    Separate load/play component into 2 separate components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import LoadAudio from './components/LoadAudio'
 import './App.css'
+import { useState } from 'react'
+import PlayAudio from './components/PlayAudio';
 
 function App() {
+  const [audio, setAudio] = useState();
+
   return (
     <>
       <h1>Audio to MIDI</h1>
-      <LoadAudio />
+      <PlayAudio audio={audio} />
+      <LoadAudio setAudio={setAudio} />
     </>
   )
 }
--- src/components/LoadAudio.jsx
+++ src/components/LoadAudio.jsx
@@ -1,37 +1,17 @@
 import React from 'react'
-import { useState, useEffect } from 'react'
 
 
-
-export default function LoadAudio(){
-
-  const [playPause, setPlayPause] = useState('play');
-  const [audio, setAudio] = useState();
-
+export default function LoadAudio(props){
 
   function addFile(e){
     console.log(e.target.files[0]);
     let s = URL.createObjectURL(e.target.files[0]);
     let a = new Audio(s);
-    setAudio(a);
+    props.setAudio(a);
   }
   
-  function handleClick(){
-    if (playPause === 'play'){
-      audio.play();
-      setPlayPause('pause');
-    }
-
-    else {
-      audio.pause();
-      setPlayPause('play');
-    }
-    
-  }
-
     return (
         <div>
-          <button onClick={handleClick}>Play/Pause</button>
           <input type="file" onChange={addFile}/>
         </div>
       );
