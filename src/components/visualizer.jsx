176c0c70d8954a0bcddfa4285672d4a10c42500e
Michael Hirt <hirtm14@gmail.com>
2023-05-12 13:14:36 -0400

    Working visualizer

--- src/components/visualizer.jsx
+++ src/components/visualizer.jsx
@@ -1,15 +1,15 @@
 import { WaterfallSVGVisualizer } from "@magenta/music/es6";
 import { useRef } from "react";
 
-const container = <div></div>;
+const container = <div id='visualizer'></div>;
 
 export default function Visualizer(props){
     if(props.ns === undefined){return;}
 
 
-    const vis = new WaterfallSVGVisualizer(props.ns, container)
+    const vis = new WaterfallSVGVisualizer(props.ns, document.getElementById('canvas'))
     console.log(vis);
     return(
-        {container}
+        vis
     );
 }
