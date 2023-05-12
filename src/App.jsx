b6e9a4cbd1abeb70a284c82a710c1501b177090a
Michael Hirt <hirtm14@gmail.com>
2023-05-12 13:20:28 -0400

    Move visualization to App.js, fixes visualizer deleting other elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import LoadAudio from './components/LoadAudio'
 import './App.css'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import PlayAudio from './components/PlayAudio';
 import PlayBackRate from './components/PlaybackRate';
 // import Magenta from './components/magenta';
 import Visualizer from './components/visualizer';
+import { WaterfallSVGVisualizer } from '@magenta/music/es6';
 
 
 function App() {
   const [audio, setAudio] = useState();
   const [rate, setRate] = useState(1);
-  const [noteSequence, setNoteSequence] = useState();
+  const [noteSequence, setNoteSequence] = useState(null);
+
+  function visualize(){
+    let vis = new WaterfallSVGVisualizer(noteSequence, document.getElementById('visualizer'))
+  }
+
+  useEffect(()=>{
+    if(noteSequence === null){return;}
+    visualize();
+  }, [noteSequence])
 
   return (
     <>
@@ -18,7 +28,8 @@ function App() {
       <PlayAudio audio={audio} />
       <LoadAudio setAudio={setAudio} setNoteSequence={setNoteSequence} />
       <PlayBackRate audio={audio} setRate={setRate} />
-      <Visualizer ns={noteSequence} />
+      {/* <Visualizer ns={noteSequence} /> */}
+      <div id='visualizer'></div>
       {/* <Magenta /> */}
     </>
   )
