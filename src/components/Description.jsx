1faa6a5db5f1ab2ce7a2951b21a21a9c11af2598
Michael Hirt <hirtm14@gmail.com>
2023-05-16 19:20:41 -0400

    Edit description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -3,9 +3,9 @@ export default function Description(){
         <div id='description'>
             <p>Upload an audio file and have it converted into MIDI with controllable playback!
             <br></br><br></br>
-                The neural network model used was trained on solo piano performances,
-                so that is what will give you the best results! However, feel free 
-                to upload audio of other instruments to see what happens 😀
+            The neural network model used was trained on solo piano performances,
+            so that is what will give you the best results! But feel free 
+            to upload audio of other instruments to see what happens! 😀
             <br></br><br></br>
             Please be patient after selecting your file. The longer the audio, the longer it takes to process!
             </p>
