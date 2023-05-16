d14d88b48e448f2eb562771607b4310b32442ace
Michael Hirt <hirtm14@gmail.com>
2023-05-16 19:28:59 -0400

    Change description grammar

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -4,7 +4,7 @@ export default function Description(){
             <p>Upload an audio file and have it converted into MIDI with controllable playback!
             <br></br><br></br>
             The neural network model used was trained on solo piano performances,
-            so that is what will give you the best results, but feel free 
+            so those are what will give you the best results, but feel free 
             to upload audio of other instruments to see what happens! 😀
             <br></br><br></br>
             Please be patient after selecting your file. The longer the audio, the longer it takes to process!
