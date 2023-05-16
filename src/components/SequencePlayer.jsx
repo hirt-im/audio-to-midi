b70836f8685dbb45cf6b320dce1294dd4c52eb3b
Michael Hirt <hirtm14@gmail.com>
2023-05-16 13:25:01 -0400

    Use state to show play/pause depending on playState of player

--- src/components/SequencePlayer.jsx
+++ src/components/SequencePlayer.jsx
@@ -1,24 +1,30 @@
+import { useState } from "react";
 
 export default function SequencePlayer(props){
+
+  const [playState, setPlayState] = useState(props.player.getPlayState())
 
   function handleClick(){
     switch (props.player.getPlayState()) {
       case 'stopped':
         props.player.start(props.vis.noteSequence);
+        setPlayState('started');
         break;
 
       case 'paused':
         props.player.resume();
+        setPlayState('started');
         break;
 
       case 'started':
         props.player.pause();
+        setPlayState('paused');
     }
   }
 
   return (
       <div>
-        <button onClick={handleClick}>Play/Pause</button>
+        <button onClick={handleClick}>{(playState === 'stopped' || playState === 'paused' ? 'Play' : 'Pause')}</button>
       </div>
     );
 }
