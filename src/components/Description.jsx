dbc6f01e176e3ca9508a0f8fbe80bd209b5608f5
Michael Hirt <hirtm14@gmail.com>
2023-05-16 19:34:43 -0400

    Edit description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,7 @@
 export default function Description(){
     return(
         <div id='description'>
-            <p>Upload an audio file and have it converted into MIDI with controllable playback!
+            <p>Upload an audio file and have it converted into MIDI!
             <br></br><br></br>
             The neural network model used was trained on solo piano performances,
             so those are what will give you the best results, but feel free 
