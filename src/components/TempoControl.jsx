9add04051d6821899fd4b9416560a6270cea8256
Michael Hirt <hirtm14@gmail.com>
2023-05-16 22:18:58 -0400

    Remove 'playback rate' label

--- src/components/TempoControl.jsx
+++ src/components/TempoControl.jsx
@@ -14,7 +14,7 @@ export default function TempoControl(props){
     return(
         <div id='tempo-container' style={{ display: 'flex', flexDirection: 'column' }}> 
             <div style={{ display: 'flex', flexDirection: 'column' }}>
-                <div><b>Playback Rate</b></div>
+                {/* <div><b>Playback Rate</b></div> */}
                 <div>{Math.round((tempo / 120) * 100)}%</div>
             </div>
             <input type="range" min="12" max="240" value={tempo} class="slider" onChange={handleChange}/>
